880c6cfd29c754311c6a958e211639f340122310
EmosewaPixel <awesomeggaming@gmail.com>
2019-09-23 21:43:37 +0200

    Backported to 1.12.2

--- src/main/java/com/emosewapixel/giftfromthegods/ModConfig.java
+++ src/main/java/com/emosewapixel/giftfromthegods/ModConfig.java
@@ -1,21 +1,19 @@
 package com.emosewapixel.giftfromthegods;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.config.Config;
 
-import java.util.Collections;
-import java.util.List;
-
+@Config(modid = GiftFromTheGods.MODID)
 public class ModConfig {
-	public static final ForgeConfigSpec COMMON;
-	public static final ForgeConfigSpec.IntValue time;
-	public static final ForgeConfigSpec.ConfigValue<List<? extends String>> blackList;
-	public static final ForgeConfigSpec.BooleanValue invertBlackList;
+	@Config.Comment("Sets how much time (in ticks) is needed for players to get their next random item")
+	@Config.Name("Delay")
+	@Config.RangeInt(min = 1)
+	public static int time = 200;
 	
-	static {
-		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-		time = builder.comment("Sets how much time (in ticks) is needed for players to get their next random item").defineInRange("Delay", 200, 1, Integer.MAX_VALUE);
-		blackList = builder.comment("List of items that won't given to players").defineList("Blacklist", Collections.emptyList(), s -> s instanceof String);
-		invertBlackList = builder.comment("When enabled, the blacklist becomes a whitelist").define("Invert Blacklist", false);
-		COMMON = builder.build();
-	}
+	@Config.Comment("List of items that won't given to players")
+	@Config.Name("Blacklist")
+	public static String[] blackList = new String[0];
+	
+	@Config.Comment("When enabled, the blacklist becomes a whitelist")
+	@Config.Name("Invert Blacklist")
+	public static boolean invertBlackList = false;
 }
